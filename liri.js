92c310fd11d1694b595cd3983ff00ac3320fbc66
Katie Lewis <klewi314@gmail.com>
2019-04-17 19:49:02 -0700

    Added functions

--- liri.js
+++ liri.js
@@ -1,1 +1,22 @@
 require("dotenv").config();
+
+//Commands
+//Concert-this
+//Spotify-this-song
+//movie-this
+//do-what-it-says
+const runApp = (command, input) => {
+  switch (command) {
+    case "concert-this":
+      getArtist(input);
+      break;
+    case "spotify-this-song":
+      break;
+    case "movie-this":
+      break;
+    case "do-what-it-says":
+      break;
+  }
+};
+
+getArtist = () => {};
